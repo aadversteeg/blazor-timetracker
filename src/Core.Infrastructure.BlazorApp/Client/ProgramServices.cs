3ad6f8420e496349cb8d45d173227d7798be092b
Aad Versteeg <aad.versteeg@transavia.com>
2022-11-28 22:04:46 +0100

    Added filtering of projects

--- src/Core.Infrastructure.BlazorApp/Client/ProgramServices.cs
+++ src/Core.Infrastructure.BlazorApp/Client/ProgramServices.cs
@@ -1,19 +1,25 @@
 ﻿using Microsoft.JSInterop;
 using System.Xml.Linq;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.Rest;
 
 namespace Core.Infrastructure.BlazorApp.Client
 {
     public class ProgramServices
     {
         private readonly IJSRuntime _jsRuntime;
+        private readonly IAccessTokenProvider _tokenProvider;
 
-        public ProgramServices(IJSRuntime jsRuntime) { 
+
+		public ProgramServices(IJSRuntime jsRuntime, IAccessTokenProvider tokenProvider) { 
             _jsRuntime = jsRuntime;
+            _tokenProvider = tokenProvider; 
         }
         public  async Task<bool> Confirm(string message)
         {
             return await _jsRuntime.InvokeAsync<bool>("confirm", message);
         }
-
     }
 }
